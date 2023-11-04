e7923002ca859bfaa2f89912cbd783793b720e71
DevOps Easy Learning <support@devopseasylearning.com>
2023-11-04 16:10:11 -0500

    sed

--- linux/sed/script.sh
+++ linux/sed/script.sh
@@ -23,3 +23,11 @@ sed -i "s/POSTGRES_PASSWORD_CRED/$POSTGRES_PASSWORD_CRED/g" a.yaml
 sed -i "s/MYSQL_ROOT_PASSWORD_CRED/$MYSQL_ROOT_PASSWORD_CRED/g" a.yaml
 
 cat a.yaml
+
+## =================================================================
+sed -i "s|POSTGRES_VERSION|v1.1.0|g" a.yaml
+sed -i "s|MYSQL_VERSION|v1.1.0|g" a.yaml
+sed -i "s|POSTGRES_PASSWORD_CRED|$POSTGRES_PASSWORD_CRED|g" a.yaml
+sed -i "s|MYSQL_ROOT_PASSWORD_CRED|$MYSQL_ROOT_PASSWORD_CRED|g" a.yaml
+
+cat a.yaml
